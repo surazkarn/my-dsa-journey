b21648ab33649feddfa83b0eeaa5c11042bf6639
surazkarn <surazkarn8848@gmail.com>
2022-09-30 01:16:10 +0530

    reverse

--- Maths/reverseNum.java
+++ Maths/reverseNum.java
@@ -1,8 +1,24 @@
-package Maths;
 
-public class reverseNum {
+//Given a number N reverse the number and print it.
+public class reversenum {
+
+    public static int rev(int n) {
+
+        int temp = 0;
+        while (n != 0) {
+            int rem = n % 10; // 134/10 then rem -> 4
+
+            temp = temp * 10 + rem; // 40
+            n = n / 10;
+
+        }
+
+        return temp;
+    }
 
     public static void main(String[] args) {
+        int n = 13456789;
+        System.out.println(rev(n));
 
     }
 
