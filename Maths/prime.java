f79f45cc2dd33740a669fa6f30e97f5ca695ff54
surazkarn <surazkarn8848@gmail.com>
2022-09-30 21:25:19 +0530

    divisor

---
+++ Maths/prime.java
@@ -0,0 +1,13 @@
+// Given a number, check whether it is prime or not. 
+//A prime number is a natural number that is only divisible by 1 and by itself.
+
+public class prime {
+    public static void main(String[] args) {
+        int n = 3;
+
+        for (int i = 2; i < n - 1; i++) {
+
+        }
+    }
+
+}
