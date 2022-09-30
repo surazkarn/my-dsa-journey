0955d90fa691755e8183d65dcc937a29887488bb
surazkarn <surazkarn8848@gmail.com>
2022-09-30 11:20:07 +0530

    gcd

--- Maths/gcdTwo.java
+++ Maths/gcdTwo.java
@@ -1,12 +1,20 @@
+import java.util.Scanner;
+
 public class gcdTwo {
-
-    public static int gcdofTwoNum(int n) {
-
-    }
-
     public static void main(String[] args) {
-        int n1 = 8;
-        int n2 = 10;
+
+        Scanner sc = new Scanner(System.in);
+
+        int divident = sc.nextInt();
+        int divisor = sc.nextInt();
+
+        while (divident % divisor != 0) {
+            int rem = divident % divisor;
+            divident = divisor;
+            divident = rem;
+        }
+        System.out.println(divisor);
+        System.out.println();
 
     }
 
