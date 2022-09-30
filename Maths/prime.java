848bdb905028c988a87265f74249d88ad076236d
surazkarn <surazkarn8848@gmail.com>
2022-09-30 21:44:46 +0530

    prime

--- Maths/prime.java
+++ Maths/prime.java
@@ -3,11 +3,22 @@
 
 public class prime {
     public static void main(String[] args) {
-        int n = 3;
+        int n = 26;
+        boolean flag = true;
 
-        for (int i = 2; i < n - 1; i++) {
+        for (int i = 2; i <= n - 1; i++) {
+
+            if (n % i == 0) {
+                flag = false;
+            }
+        }
+
+        if (n != 1 && flag == true) {
+            System.out.println("prime");
+
+        } else {
+            System.out.println("not prime");
 
         }
     }
-
 }
